fea9e3bf40b8a0a053a5ac6b459f8eb6334d0858
Kevin Mensah <kevinmensah114@gmail.com>
2023-01-18 18:00:59 +0800

    Sheesh

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-<table id="example" class="table table-striped" style="width:100%">
+    <table id="example" class="table table-striped" style="width:100%">
         <thead>
             <tr>
                 <th>Name</th>
@@ -10,61 +10,75 @@
         </thead>
         <tbody>
         </tbody>
-</table>
+    </table>
 
     <script type="module">
         $(document).ready(function() {
 
-            var data = [
-                {
-                    "name":       "Tiger Nixon",
-                    "position":   "System Architect",
-                    "salary":     "3,120",
+            var data = [{
+                    "name": "Tiger Nixon",
+                    "position": "System Architect",
+                    "salary": "3,120",
                     "start_date": "2011/04/25",
-                    "office":     "Edinburgh",
-                    "extn":       "5421"
+                    "office": "Edinburgh",
+                    "extn": "5421"
                 },
                 {
-                    "name":       "Garrett Winters",
-                    "position":   "Director",
-                    "salary":     "5,300",
+                    "name": "Garrett Winters",
+                    "position": "Director",
+                    "salary": "5,300",
                     "start_date": "2011/07/25",
-                    "office":     "Edinburgh",
-                    "extn":       "8422"
+                    "office": "Edinburgh",
+                    "extn": "8422"
                 }
             ]
             $('#example').DataTable({
                 data: data,
-                lengthMenu: [[10, 25, 50, -1], [10, 25, 50, 'All']],
-                columns: [
-                    { data: 'name' },
-                    { data: 'position' },
-                    { 
-                        data: 'salary',
-                        render: function ( data, type, row ) {
-                            if(data == '3,120'){
-                                return '$'+ data;
-                            }else{
-                                return data;
-                            }
-                        } 
+                lengthMenu: [
+                    [10, 25, 50, -1],
+                    [10, 25, 50, 'All']
+                ],
+                language : {
+				    sLengthMenu: "Show _MENU_"
+			    },
+                columns: [{
+                        data: 'name'
                     },
-                    { 
+                    {
+                        data: 'position'
+                    },
+                    {
+                        data: 'salary',
+                        // render: function(data, type, row) {
+                        //     if (data == '3,120') {
+                        //         return '$' + data;
+                        //     } else {
+                        //         return data;
+                        //     }
+                        // }
+                    },
+                    {
                         data: 'office',
                     }
                 ],
                 columnDefs: [
                     {
                         targets: [0, 3],
-                        visible: false,
+                        visible: true,
                         searchable: false
-                    },
+                    }, 
+                    {
+                        targets: 2,
+                        render: function(data, type, row) {
+                            if (data == '3,120') {
+                                return '$' + data;
+                            } else {
+                                return data;
+                            }
+                        }
+                    }
                 ]
             });
-
-            $('#23').click(function() {
-                console.log('PARAGRAPH CLICKED');
-            })
         })
     </script>
 </x-layout>
